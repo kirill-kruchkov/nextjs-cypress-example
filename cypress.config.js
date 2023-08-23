8bdf359b7b151abe4c26a55f74578c8d1f6ab462
Kirill K <kk@kirill-k.pro>
2023-08-23 13:40:49 +0200

    make mockttp work in cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,37 @@
 const { defineConfig } = require("cypress");
+const webpackPreprocessor = require("@cypress/webpack-preprocessor");
+const webpack = require("webpack");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const webpackOptions = {
+        resolve: {
+          fallback: {
+            zlib: false,
+            fs: false,
+            path: false,
+            querystring: false,
+            url: require.resolve("url"),
+            stream: require.resolve("stream-browserify"),
+            buffer: require.resolve("buffer"),
+          },
+        },
+        experiments: {
+          asyncWebAssembly: true,
+        },
+        plugins: [
+          new webpack.ProvidePlugin({ process: "process/browser" }),
+          new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+          })
+        ]
+      }
+      on('file:preprocessor', webpackPreprocessor({ webpackOptions }))
+      return config
     },
   },
 });
+
+
