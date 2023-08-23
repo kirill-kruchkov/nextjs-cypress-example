85846ea4b99b1dd007d150938af97cee79bb2b59
Kirill K <kk@kirill-k.pro>
2023-08-23 17:28:22 +0200

    assert on requests sent by the server

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,11 +2,11 @@ import { getRemote } from 'mockttp'
 
 const server = getRemote()
 
-before(() => {
+beforeEach(() => {
   cy.wrap(server.start(9999))
 })
 
-after(() => {
+afterEach(() => {
   cy.wrap(server.stop())
 })
 
@@ -14,4 +14,16 @@ it('renders the JSON', () => {
   cy.wrap(server.forGet("/jsonplaceholder/todos/1").always().thenJson(200, { userId: 2 }))
   cy.visit('/')
   cy.contains('UserID: 2').should('be.visible')
+})
+
+it('sends a correct request', () => {
+  cy.wrap(server.forGet("/jsonplaceholder/todos/1").always().thenJson(200, { userId: 2 }))
+    .as('mockEndpoint')
+  cy.visit('/')
+  cy.get('@mockEndpoint').then(mockEndpoint => {
+    cy.wrap(mockEndpoint?.getSeenRequests()).then(reqs => {
+      cy.wrap(reqs).should('have.length', 1)
+      cy.wrap(reqs[0]).its('headers').should('have.property', 'accept')
+    })
+  })
 })
